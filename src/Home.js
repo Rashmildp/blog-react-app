75d4733adb09cbf488eb16c8392bec833ae95ffb
Rashmildp <rashmildp@gamil.com>
2022-01-31 15:31:44 +0530

    add-useFetch-file

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,19 @@
-import { useEffect, useState } from "react";
+
 import BlogList from "./blogList";
+import useFetch from "./useFetch";
 
 const Home=()=>{
-    const [name, setName]=useState('Rashmi');
-   const [age,setAge]=useState(25);
-   const [blogs,setblogs]=useState([
-       {title:'Hello world', body: 'Helll sxxvvf dfdts', author:'Rashmi', id:1},
-       {title:'Welcome to the RecatJs', body: 'React is very popular frontend library', author:'Rashmi', id:2},
-       {title:'Welcome to the .NET', body: 'New version is .NET6', author:'Duleesha', id:3},
-   ])
-    const handleClick=()=>{
-      
-    }
-    const handleDelete=(id)=>{
-
-        const newBlogs=blogs.filter(blog=>blog.id!==id);
-        setblogs(newBlogs);
-    }
-   useEffect(()=>{
-       console.log('Use effect run');
-       console.log(blogs);
-   },[])
+    const {data:blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
+   
+ 
+  
+ 
     return(
         <div className="home">
+            {error && <div> {error}</div>}
+            {isPending&& <div>Loading...</div>}
     
-         <BlogList blogs={blogs} title="All Blogs"  handleDelete={handleDelete}/>
-         
+       {blogs&&<BlogList blogs={blogs} title="All Blogs" />}  
          
       
            
